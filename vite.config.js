450fc12480b77c9b8485f90b182c438c3ea7a9e6
Wahid Husain <wahidhusaindev@gmail.com>
2025-08-16 08:55:02 +0000

    config changes

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-  base: "wahid-portfolio/", // Adjusted base path for GitHub Pages
+  base: "/wahid-portfolio/", // Correct base path for GitHub Pages
 })
